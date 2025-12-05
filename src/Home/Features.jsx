585e343a2e99f36d41eddf7112103e8fe9e3482e
kdu0523 <dukevin02003@gmail.com>
2025-12-05 13:39:01 -0800

    Home page layout improvements& update with mouse hover effects

--- src/Home/Features.jsx
+++ src/Home/Features.jsx
@@ -1,188 +1,73 @@
-import './feature.css';
-
-import Grid from '@material-ui/core/Grid';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Grid, Card, CardActionArea, CardContent, CardMedia, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+        flexGrow: 1,
+        padding: '4rem 2rem',
+        maxWidth: '1280px',
+        margin: '0 auto',
+    },
+    card: {
+        height: '100%',
+        transition: 'transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out',
+        '&:hover': {
+            transform: 'translateY(-5px)',
+            boxShadow: '0 10px 20px rgba(0,0,0,0.15)',
+        },
+    },
+    media: {
+        height: 200,
+        backgroundSize: 'contain',
+        margin: '1rem',
+    },
+    title: {
+        textAlign: 'center',
+        fontWeight: 600,
+        color: '#001529',
+    },
+});
+
+const featureData = [
+    { title: 'Explore Dataset', img: 'https://centilebrainwebsiteimage.s3.amazonaws.com/explore2.svg', link: '/explore' },
+    { title: 'Generate Estimates', img: 'https://centilebrainwebsiteimage.s3.amazonaws.com/iStock-1251018502%20Upload.svg', link: '/model' },
+    { title: 'CentileBrain Model', img: 'https://centilebrainwebsiteimage.s3.amazonaws.com/iStock-1155106799_Model.svg', link: '/model2' },
+    { title: 'Global BrainAGE', img: 'https://centilebrainwebsiteimage.s3.amazonaws.com/Global+BrainAGE.png', link: '/brainAGE' },
+    { title: 'Network BrainAGE', img: 'https://centilebrainwebsiteimage.s3.amazonaws.com/Network+BrainAGE.jpg', link: '/brainAGE3' },
+    { title: 'eHarmonize', img: 'https://centilebrainwebsiteimage.s3.amazonaws.com/eharmonize.JPG', link: '/eHarmonize' },
+    { title: 'FAQ', img: 'https://centilebrainwebsiteimage.s3.amazonaws.com/FAQ.png', link: '/faq' },
+    { title: 'Core Team', img: 'https://centilebrainwebsiteimage.s3.amazonaws.com/iStock-966858736_Team.svg', link: '/team' },
+    { title: 'Publications', img: 'https://centilebrainwebsiteimage.s3.amazonaws.com/iStock-1323841513%20publications.svg', link: '/publications' },
+];
 
 export default function Features() {
+    const classes = useStyles();
+
     return (
-        <div style={{ paddingLeft: '4rem', paddingRight: '4rem' }}>
-            <Grid container spacing={10} alignItems={'stretch'}>
-                <Grid item xs={12} md={4}>
-                    <Grid item xs={12}>
-                        <Link to="/explore">
-                            <img
-                                src="https://centilebrainwebsiteimage.s3.amazonaws.com/explore2.svg"
-                                width="100%"
-                            />
+        <div className={classes.root}>
+            <Grid container spacing={4} alignItems="stretch">
+                {featureData.map((item, index) => (
+                    <Grid item xs={12} sm={6} md={4} key={index}>
+                        <Link to={item.link} style={{ textDecoration: 'none' }}>
+                            <Card className={classes.card} elevation={2}>
+                                <CardActionArea>
+                                    <CardMedia
+                                        className={classes.media}
+                                        image={item.img}
+                                        title={item.title}
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h6" component="h2" className={classes.title}>
+                                            {item.title}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
                         </Link>
                     </Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        style={{ paddingTop: '1rem', textAlign: 'center' }}
-                    >
-                        <h5>Explore the CentileBrain Dataset</h5>
-                    </Grid>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <Grid item xs={12}>
-                        <Link to="/model">
-                            <img
-                                src="https://centilebrainwebsiteimage.s3.amazonaws.com/iStock-1251018502%20Upload.svg"
-                                width="100%"
-                            />
-                        </Link>
-                    </Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        style={{ paddingTop: '1rem', textAlign: 'center' }}
-                    >
-                        <h5>Use CentileBrain to Generate Estimates</h5>
-                    </Grid>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <Grid item xs={12}>
-                        <Link to="/model2">
-                            <img
-                                src="https://centilebrainwebsiteimage.s3.amazonaws.com/iStock-1155106799_Model.svg"
-                                width="100%"
-                            />
-                        </Link>
-                    </Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        style={{ paddingTop: '1rem', textAlign: 'center' }}
-                    >
-                        <h5>The CentileBrain Model</h5>
-                    </Grid>
-                </Grid>
-            </Grid>
-
-            <Grid container spacing={10} alignItems={'stretch'}>
-                <Grid item xs={12} md={4}>
-                    <Grid item xs={12}>
-                        <Link to="/brainAGE">
-                            <img
-                                src="https://centilebrainwebsiteimage.s3.amazonaws.com/Global+BrainAGE.png"
-                                width="100%"
-                            />
-                        </Link>
-                    </Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        style={{ paddingTop: '1rem', textAlign: 'center' }}
-                    >
-                        <h5>Global BrainAGE</h5>
-                    </Grid>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <Grid item xs={12}>
-                        <Link to="/brainAGE3">
-                            <img
-                                src="https://centilebrainwebsiteimage.s3.amazonaws.com/Network+BrainAGE.jpg"
-                                width="100%"
-                            />
-                        </Link>
-                    </Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        style={{ paddingTop: '1rem', textAlign: 'center' }}
-                    >
-                        <h5>Network BrainAGE</h5>
-                    </Grid>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <Grid item xs={12}>
-                        <Link to="/eHarmonize">
-                            <img
-                                src="https://centilebrainwebsiteimage.s3.amazonaws.com/eharmonize.JPG"
-                                width="100%"
-                            />
-                        </Link>
-                    </Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        style={{ paddingTop: '1rem', textAlign: 'center' }}
-                    >
-                        <h5>eHarmonize</h5>
-                    </Grid>
-                </Grid>
-            </Grid>
-
-            <Grid container spacing={10} alignItems={'stretch'}>
-                {/* <Grid item xs={12} md={2}>
-                    <Grid item xs={12}></Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        style={{ paddingTop: '1rem', textAlign: 'center' }}
-                    ></Grid>
-                </Grid> */}
-                <Grid item xs={12} md={4}>
-                    <Grid item xs={12}>
-                        <Link to="/faq">
-                            <img
-                                src="https://centilebrainwebsiteimage.s3.amazonaws.com/FAQ.png"
-                                width="100%"
-                            />
-                        </Link>
-                    </Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        style={{ paddingTop: '1rem', textAlign: 'center' }}
-                    >
-                        <h5>Frequently Asked Questions</h5>
-                    </Grid>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <Grid item xs={12}>
-                        <Link to="/team">
-                            <img
-                                src="https://centilebrainwebsiteimage.s3.amazonaws.com/iStock-966858736_Team.svg"
-                                width="100%"
-                            />
-                        </Link>
-                    </Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        style={{ paddingTop: '1rem', textAlign: 'center' }}
-                    >
-                        <h5>Core Team</h5>
-                    </Grid>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <Grid item xs={12}>
-                        <Link to="/publications">
-                            <img
-                                src="https://centilebrainwebsiteimage.s3.amazonaws.com/iStock-1323841513%20publications.svg"
-                                width="100%"
-                            />
-                        </Link>
-                    </Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        style={{ paddingTop: '1rem', textAlign: 'center' }}
-                    >
-                        <h5>CentileBrain Publications</h5>
-                    </Grid>
-                </Grid>
-                {/* <Grid item xs={12} md={2}>
-                    <Grid item xs={12}></Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        style={{ paddingTop: '1rem', textAlign: 'center' }}
-                    ></Grid>
-                </Grid> */}
+                ))}
             </Grid>
         </div>
     );

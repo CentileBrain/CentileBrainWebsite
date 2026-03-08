a997448b1bb80be3aefddc583c14c154328ce4b4
kdu0523 <dukevin02003@gmail.com>
2026-03-08 12:45:46 -0700

    PublicationsPage.jsx: Updated publication information and links to reflect the latest research findings and ensure accurate representation of our work.

--- src/Publications/PublicationsPage.jsx
+++ src/Publications/PublicationsPage.jsx
@@ -75,7 +75,7 @@ export default function PublicationsPage(props) {
             id: uuid(),
         },
         {
-            info: 'Alyssa H. Zhu, etc, Paul M. Thompson, Neda Jahanshad, Alzheimer’s Disease Neuroimaging Initiative. Lifespan reference curves for harmonizing multi-site regional brain white matter metrics from diffusion MRI. 2024; doi: 10.1101/2024.02.22.581646',
+            info: 'Wang H. R, etc, Frangou S, Thompson P, Tiffany C. Ho. Childhood Maltreatment and Deviations from Normative Brain Structure: A Mega-Analysis of 3,711 Individuals from the ENIGMA MDD and ENIGMA PTSD Working Groups. Biological Psychiatry. 2026. DOI: 10.1016/j.biopsych.2026.02.016',
             link: 'https://www.biorxiv.org/content/10.1101/2024.02.22.581646',
             id: uuid(),
         },

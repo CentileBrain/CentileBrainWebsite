b674ae3321e2d68341898fe91cd39dd2c6711241
kdu0523 <dukevin02003@gmail.com>
2026-05-20 16:51:13 -0700

    Updates

--- index.js
+++ index.js
@@ -72867,44 +72867,64 @@ webpackJsonp([1,0],[
 	    }, []);
 
 	    var publications = [{
-	        info: 'Frangou S, Modabbernia A, Williams SCR, et al. Cortical thickness across the lifespan: Data from 17,075 healthy individuals aged 3-90 years. Hum Brain Mapp. 2021;10.1002/hbm.25364. doi:10.1002/hbm.25364',
-	        link: 'https://pubmed.ncbi.nlm.nih.gov/33595143/',
+	        info: 'Ge R, Yu Y, New F, et al., Thompson PM, Frangou S. Empirical validation of race-neutral normative brain morphometry models across ethnoracially diverse populations. Proc Natl Acad Sci USA. 2026;123(20):e2521055123. doi: 10.1073/pnas.2521055123',
+	        link: 'https://www.pnas.org/doi/10.1073/pnas.2521055123',
 	        id: (0, _uuid.v4)()
 	    }, {
-	        info: 'Dima D, Modabbernia A, Papachristou E, et al. Subcortical volumes across the lifespan: Data from 18,605 healthy individuals aged 3-90 years. Hum Brain Mapp. 2021;10.1002/hbm.25320. doi:10.1002/hbm.25320',
-	        link: 'https://pubmed.ncbi.nlm.nih.gov/33570244/',
+	        info: 'Hopkins WD, Frangou S, Ge R. Brain age gap is associated with cognitive abilities in captive chimpanzees. Sci Rep. 2025;15:41984. doi: 10.1038/s41598-025-26062-5',
+	        link: 'https://www.nature.com/articles/s41598-025-26062-5',
 	        id: (0, _uuid.v4)()
 	    }, {
-	        info: 'Wierenga LM, Doucet GE, Dima D, et al. Greater male than female variability in regional brain structure across the lifespan [published online ahead of print, 2020 Oct 12]. Hum Brain Mapp. 2020;10.1002/hbm.25204. doi:10.1002/hbm.25204',
-	        link: 'https://pubmed.ncbi.nlm.nih.gov/33044802/',
+	        info: 'Yu K, Ge R, Yu Y, Haas S, Sanford N, Yatham LN, Frangou S, Chakrabarty T. Individual-level brain phenotypes in first-episode mania: normative modelling of brain morphometry and brainAGE. BJPsych Open. 2025;11(3):e95. doi: 10.1192/bjo.2025.28',
+	        link: 'https://doi.org/10.1192/bjo.2025.28',
 	        id: (0, _uuid.v4)()
 	    }, {
-	        info: 'Modabbernia A, Whalley HC, Glahn DC, Thompson PM, Kahn RS, Frangou S. Systematic evaluation of machine learning algorithms for neuroanatomically-based age prediction in youth. Hum Brain Mapp. 2022; doi:10.1002/hbm.26010',
-	        link: 'https://pubmed.ncbi.nlm.nih.gov/35852028/',
-	        id: (0, _uuid.v4)()
-	    }, {
-	        info: 'Sanford N, Ge R, Antoniades M, Modabbernia A, Haas SS, Whalley HC, Galea L, Popescu SG, Cole JH, Frangou S. Sex differences in predictors and regional patterns of brain age gap estimates. Hum Brain Mapp. 2022; doi: 10.1002/hbm.25983',
-	        link: 'https://pubmed.ncbi.nlm.nih.gov/35790053/',
-	        id: (0, _uuid.v4)()
-	    }, {
-	        info: 'Ge R, Yu Y, Qi Y, Haas S, etc, Thompson P, Frangou S. Normative Modeling of Brain Morphometry Across the Lifespan using CentileBrain: Algorithm Benchmarking and Model Optimization. Lancet Digit Health. 2024; doi: 10.1016/S2589-7500(23)00250-9',
+	        info: 'Ge R, Yu Y, Qi Y, Haas S, et al., Thompson PM, Frangou S. Normative Modeling of Brain Morphometry Across the Lifespan using CentileBrain: Algorithm Benchmarking and Model Optimization. Lancet Digit Health. 2024;6(3):e211-e221. doi: 10.1016/S2589-7500(23)00250-9',
 	        link: 'https://doi.org/10.1016/S2589-7500(23)00250-9',
 	        id: (0, _uuid.v4)()
 	    }, {
-	        info: 'Haas S, Ge R, etc, Frangou S. Normative modeling of brain morphometry in Clinical High-Risk for Psychosis. JAMA Psychiatry. 2024; doi: 10.1001/jamapsychiatry.2023.3850',
+	        info: 'Haas S, Ge R, et al., Frangou S. Normative modeling of brain morphometry in Clinical High-Risk for Psychosis. JAMA Psychiatry. 2024;81(1):77-88. doi: 10.1001/jamapsychiatry.2023.3850',
 	        link: 'https://www.jamanetwork.com/journals/jamapsychiatry/fullarticle/2810624',
 	        id: (0, _uuid.v4)()
 	    }, {
-	        info: 'Yu Y, Cui H, etc, Thompson P, Frangou S, Ge R. Brain-Age Prediction: Systematic Evaluation of Site Effects, and Sample Age Range and Size. Human Brain Mapping. 2024; doi: 10.1002/hbm.26768',
+	        info: 'Yu Y, Cui H, et al., Thompson PM, Frangou S, Ge R. Brain-Age Prediction: Systematic Evaluation of Site Effects, and Sample Age Range and Size. Human Brain Mapping. 2024;45(10):e26768. doi: 10.1002/hbm.26768',
 	        link: 'https://onlinelibrary.wiley.com/doi/10.1002/hbm.26768',
 	        id: (0, _uuid.v4)()
 	    }, {
-	        info: 'Alyssa H. Zhu, etc, Paul M. Thompson, Neda Jahanshad, Alzheimer’s Disease Neuroimaging Initiative. Lifespan reference curves for harmonizing multi-site regional brain white matter metrics from diffusion MRI. 2024; doi: 10.1101/2024.02.22.581646',
-	        link: 'https://www.biorxiv.org/content/10.1101/2024.02.22.581646',
+	        info: 'Sanford N, Ge R, Antoniades M, Modabbernia A, Haas SS, Whalley HC, Galea L, Popescu SG, Cole JH, Frangou S. Sex differences in predictors and regional patterns of brain age gap estimates. Hum Brain Mapp. 2022;43(15):4689-4698. doi: 10.1002/hbm.25983',
+	        link: 'https://onlinelibrary.wiley.com/doi/10.1002/hbm.25983',
 	        id: (0, _uuid.v4)()
 	    }, {
-	        info: 'Wang H. R, etc, Frangou S, Thompson P, ... , Tiffany C. Ho. Childhood Maltreatment and Deviations from Normative Brain Structure: A Mega-Analysis of 3,711 Individuals from the ENIGMA MDD and ENIGMA PTSD Working Groups. Biological Psychiatry. 2026. DOI: 10.1016/j.biopsych.2026.02.016',
-	        link: 'https://www.biologicalpsychiatryjournal.com/article/S0006-3223(26)00093-4/fulltext',
+	        info: 'Modabbernia A, Whalley HC, Glahn DC, Thompson PM, Kahn RS, Frangou S. Systematic evaluation of machine learning algorithms for neuroanatomically-based age prediction in youth. Hum Brain Mapp. 2022;43(17):5126-5140. doi:10.1002/hbm.26010',
+	        link: 'https://onlinelibrary.wiley.com/doi/10.1002/hbm.26010',
+	        id: (0, _uuid.v4)()
+	    }, {
+	        info: 'Frangou S, Modabbernia A, Williams SCR, et al., Thompson PM, Dima D. Cortical thickness across the lifespan: Data from 17,075 healthy individuals aged 3-90 years. Hum Brain Mapp. 2021;43(1):431-451. doi:10.1002/hbm.25364',
+	        link: 'https://onlinelibrary.wiley.com/doi/10.1002/hbm.25364',
+	        id: (0, _uuid.v4)()
+	    }, {
+	        info: 'Dima D, Modabbernia A, Papachristou E, et al., Thompson PM, Frangou S.  Subcortical volumes across the lifespan: Data from 18,605 healthy individuals aged 3-90 years. Hum Brain Mapp. 2021;43(1):452-469. doi:10.1002/hbm.25320',
+	        link: 'https://onlinelibrary.wiley.com/doi/10.1002/hbm.25320',
+	        id: (0, _uuid.v4)()
+	    }, {
+	        info: 'Zhang X, Yu Y, Liu X, Fang Y, Wang J, Frangou S, Zang Y, Ge R, Zhang H. Medication and Developmental Stage Shape the Brain-Age Gap in ADHD. BioRXiv. doi: 10.64898/2026.01.19.700322',
+	        link: 'https://doi.org/10.64898/2026.01.19.700322',
+	        id: (0, _uuid.v4)()
+	    }, {
+	        info: 'Wang HR, et al., Frangou S, Thompson PM, ... , Ho TC. Childhood Maltreatment and Deviations from Normative Brain Structure: A Mega-Analysis of 3,711 Individuals from the ENIGMA MDD and ENIGMA PTSD Working Groups. Biological Psychiatry. 2026. doi: 10.1016/j.biopsych.2026.02.016',
+	        link: 'https://doi.org/10.1016/j.biopsych.2026.02.016',
+	        id: (0, _uuid.v4)()
+	    }, {
+	        info: 'Bernardoni F, Arold D, Schoppik L, Bahnsen K, Ge R, et al., Frangou S, King JA, Thompson PM, Ehrlich S. Brain morphology in anorexia nervosa and its subtypes: A multi-cohort study of individual participant data. PLoS Med. 2026;23(5):e1004809. doi: 10.1371/journal.pmed.1004809',
+	        link: 'https://doi.org/10.1371/journal.pmed.1004809',
+	        id: (0, _uuid.v4)()
+	    }, {
+	        info: 'Zhu AH, et al., Thompson PM, Jahanshad N. Alzheimer’s Disease Neuroimaging Initiative. Lifespan reference curves for harmonizing multi-site regional brain white matter metrics from diffusion MRI. Sci Data. 2025;12:748. doi: 10.1038/s41597-025-05028-2',
+	        link: 'https://www.nature.com/articles/s41597-025-05028-2',
+	        id: (0, _uuid.v4)()
+	    }, {
+	        info: 'Wierenga LM, Doucet GE, Dima D, et al., Frangou S, Tamnes CK. Greater male than female variability in regional brain structure across the lifespan [published online ahead of print, 2020 Oct 12]. Hum Brain Mapp. 2022;43(1):470-499. doi:10.1002/hbm.25204',
+	        link: 'https://onlinelibrary.wiley.com/doi/10.1002/hbm.25204',
 	        id: (0, _uuid.v4)()
 	    }];
 
